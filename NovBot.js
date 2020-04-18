290b7992d079e335d894749b8315571d7e908940
Bitl <bitlblox@gmail.com>
2020-04-18 14:01:30 -0700

    Update NovBot.js

--- NovBot.js
+++ NovBot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.login(process.env.BOT_TOKEN);
+
 client.on('message', message => {
 	if (message.content.includes(process.env.WEB))
 	{
-		client.channels.get(process.env.STAFF_CHANNEL_ID).send('NOTICE: Potentially underage user detected. Name: '.concat(message.author.username,'#',message.author.tag,' | Channel: #',message.guild.channels.get(message.channel.id).name))
+		client.channels.cache.get(process.env.STAFF_CHANNEL_ID).send('NOTICE: Potentially underage user detected. Name: '.concat(message.author.username,'#',message.author.tag,' | Channel: #',message.guild.channels.get(message.channel.id).name))
 	}
 });
-
-client.login(process.env.BOT_TOKEN);
-//BOT_TOKEN is the Client Secret

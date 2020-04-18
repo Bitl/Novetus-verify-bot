32f1cbf1535c69f80673ee7227b9d1d9a7b8d76f
Michael Hart <bitlblox@gmail.com>
2020-04-18 13:34:59 -0700

    Add files via upload

---
+++ NovBot.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+	if (message.content.includes(process.env.WEB))
+	{
+		message.guild.channels.get(process.env.STAFF_CHANNEL_ID).send('NOTICE: Potentially underage user detected. Name: '.concat(message.author.username,'#',message.author.tag,' | Channel: #',message.guild.channels.get(message.channel.id).name))
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
+//BOT_TOKEN is the Client Secret

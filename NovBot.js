c6a0b81c9e04249ff636a07910844ec6caa8c13c
Bitl <bitlblox@gmail.com>
2020-05-29 11:27:57 -0700

    Update NovBot.js

--- NovBot.js
+++ NovBot.js
@@ -1,15 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN);
+client.login("TOKEN");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-	if (message.content.includes(process.env.WEB))
+	if (message.content.includes("MESSAGE CONTENT"))
 	{
-		client.channels.cache.get(process.env.STAFF_CHANNEL_ID).send('NOTICE: Potentially underage user detected. Name: '.concat(message.author.tag,' | Channel: #',client.channels.cache.get(message.channel.id).name))
+		const channel = message.client.channels.cache.find(ch => ch.id === 'CHANNEL ID');
+		if (!channel) 
+		{
+			console.error('Channel not in cache');
+			return;
+		}
+		
+		const channel2 = message.client.channels.cache.find(ch => ch.id === message.channel.id);
+		if (!channel2)
+		{
+			console.error('Channel not in cache');
+			return;
+		}
+		
+		channel.send('NOTICE: Potentially underage user detected. Name: '.concat(message.author.tag,' | Channel: #',channel2.name));
+	}
+	
+	if (message.channel.id == RECIEVINGCHANNELIDLONG && message.content.includes('[PUBLIC ANNOUNCEMENT]'))
+	{
+		const channel = message.client.channels.cache.find(ch => ch.id === 'CHANNEL ID');
+		if (!channel) 
+		{
+			console.error('Channel not in cache');
+			return;
+		}
+		
+		channel.send('Announcement from: '.concat(message.author.username,': ', message.content))
 	}
 });
